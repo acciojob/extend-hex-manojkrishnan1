20fe1aa9e3a55818cefaff886ce30cde01349707
manojkrishnan1 <127621202+manojkrishnan1@users.noreply.github.com>
2023-11-17 18:11:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const extendHex = (shortHex) => {
   // write your code here
+	const str = shortHex.toLowerCase();
+  let n = str.length;
+  let result = "";
+  for(let i=0;i<n;i++){
+    if(str[i]=="#")
+      continue;
+
+      result += str[i]+str[i];
+  }
+  return result;
 };
 
 // Do not change the code below.
